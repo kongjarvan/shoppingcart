c1c0d02d295e90c6d33cb4eb86819e1643b9cba4
kongjarvan <wjsdudwox@gmail.com>
2022-10-06 09:51:05 +0900

    기본페이지 작성 및 사진 추가

---
+++ lib/components/shoppingcart_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ShoppingCartDetail extends StatelessWidget {
+  const ShoppingCartDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:shoppingcart/components/shoppingcart_detail.dart';
+import 'package:shoppingcart/components/shoppingcart_header.dart';
+import 'package:shoppingcart/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,72 +15,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // const 붙이면 싱글톤 패턴을 따로 구현할 필요가 없음
-      home: HomePage(),
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
     );
   }
 }
 
-// 첫페이지
-class HomePage extends StatelessWidget {
-  // final을 붙이면 상태가 없는 변수가 됨 (상수)
-
-  const HomePage({Key? key}) : super(key: key);
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print("HomePage 실행됨");
     return Scaffold(
+      appBar: _buildShippingCartAppBar(),
       body: Column(
         children: [
-          DayComponent(),
-          Container(
-            color: Colors.blue,
-            height: 200,
-            child: Text(
-              "밤",
-              style: TextStyle(fontSize: 20),
-            ),
-          )
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
         ],
       ),
     );
   }
-}
 
-class DayComponent extends StatefulWidget {
-  const DayComponent({Key? key}) : super(key: key);
-
-  @override
-  State<DayComponent> createState() => _DayComponentState();
-}
-
-class _DayComponentState extends State<DayComponent> {
-  String upData = "낮";
-
-  @override
-  Widget build(BuildContext context) {
-    print("실행됨");
-    return Row(
-      children: [
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                upData = "해";
-              }); // setState로 값 변경시, 변경 후 화면 다시 그림
-              print(upData);
-            },
-            child: Text("클릭")),
-        Container(
-          color: Colors.yellow,
-          height: 200,
-          child: Text(
-            "${upData}",
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ],
+  AppBar _buildShippingCartAppBar() {
+    return AppBar(
+      title: Text("ShoppingCart"),
     );
   }
 }
